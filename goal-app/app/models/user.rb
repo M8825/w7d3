b18ccac301d29b1cbbf509f0b33b3a1580cbaf0f
Mlkz <maho.mamulashvili@gmail.com>
2022-12-21 17:06:28 -0500

    add is_password? and ensure_session_token

--- goal-app/app/models/user.rb
+++ goal-app/app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def password=(new_pass)
+    self.password_digest = BCrypt::Password.create(new_pass)
+    @password = new_pass
+  end
+
+  def is_password?(new_pass)
+    password_obj = BCrypt::Password.new(self.password_digest)
+    password_obj.is_password?(new_pass)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
 end
--- goal-app/spec/models/user_spec.rb
+++ goal-app/spec/models/user_spec.rb
@@ -1,9 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password_digest) }
   it { should validate_presence_of(:session_token) }
+
+  it { should validate_length_of(:password).is_at_least(6) }
+
+  describe "is_password?" do
+    let!(:user) { create(:user) }
+
+    context "with a valid password" do
+      it "should return true" do
+        expect(user.is_password?("password")).to be true
+      end
+    end
+  end
 end
-
-
