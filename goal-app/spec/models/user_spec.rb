d9aac3d4ad9047d29079e4dd214c7eafd74f5bf2
chakhoic <chakhoichan@gmail.com>
2022-12-21 17:28:55 -0500

    user spec

--- goal-app/spec/models/user_spec.rb
+++ goal-app/spec/models/user_spec.rb
@@ -15,5 +15,24 @@ RSpec.describe User, type: :model do
         expect(user.is_password?("password")).to be true
       end
     end
+    context "with an invalid password" do
+      it "should return false" do
+        expect(user.is_password?("bananana")).to be false
+      end
+    end
+  end
+
+  describe "password hashing" do
+    it 'does not save passwords to the database' do
+      FactoryBot.create(:user, username: "Harry Potter")
+
+      user = User.find_by(username: "Harry Potter")
+      expect(user.password).not_to eq('password')
+    end
+
+    it 'hashes password using BCrypt' do
+      expect(BCrypt::Password).to receive(:create).with('abcdef')
+      FactoryBot.build(:user, password: 'abcdef')
+    end
   end
 end
